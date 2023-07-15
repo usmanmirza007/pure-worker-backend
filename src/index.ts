cd0b4bd4544d37e11cb0a520dcac1bc8b6e138a8
Muhammad Bilal Mirza <bilal.tech1006@gmail.com>
2023-07-15 13:49:46 +0500

    Add upload static path

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,38 @@
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import express from 'express';
-import morgan from 'morgan';
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
 
-
-import { services } from './services';
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('../swagger_output.json')
-const swaggerJsdoc = require('swagger-jsdoc');
+import { services } from "./services";
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("../swagger_output.json");
+const swaggerJsdoc = require("swagger-jsdoc");
+import path from "path";
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev', {
-    skip: function (req: any, res: any) { return res.statusCode < 400 }
-  }))
-  app.use(morgan('dev', {
-    skip: function (req: any, res: any) { return res.statusCode >= 400 }
-  }))
-
-app.use('/api', services);
+app.use(
+  morgan("dev", {
+    skip: function (req: any, res: any) {
+      return res.statusCode < 400;
+    },
+  })
+);
+app.use(
+  morgan("dev", {
+    skip: function (req: any, res: any) {
+      return res.statusCode >= 400;
+    },
+  })
+);
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/api", services);
 
 const port = 8000;
 
 app.listen(port, () =>
-    console.log(`Express app listening on localhost:${port}`)
+  console.log(`Express app listening on localhost:${port}`)
 );
